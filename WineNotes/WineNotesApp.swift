bce4f2850f79dc2601869bb65916dfbf63cd8ada
ParadiseDevil <93950967+ParadiseDevil@users.noreply.github.com>
2025-08-21 23:24:06 +0500

    Initial Commit

---
+++ WineNotes/WineNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineNotesApp.swift
+//  WineNotes
+//
+//  Created by Дамир Шаймуханбетов on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct WineNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
